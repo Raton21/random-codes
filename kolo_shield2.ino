a03ef191c9fe26d51c9ed3cbd8f90985756408d7
Raton21 <radektonar@gmail.com>
2022-01-31 19:40:54 +0100

    L298P as solid state relay arduino
    Using Motor Shield L298P to control LED strip

---
+++ kolo_shield2.ino
@@ -0,0 +1,66 @@
+/*
+ * Code for exhibition of animated film in Prague, Czech Republic.
+ * 
+ * Using Motor Shield L298P controld 2 LED strips. 
+ * 
+ * After fallink pulse, turns LED for specified time
+ */
+#define LEDPulze 1300  //LED on time (in microseconds)
+
+
+#define sensorPin 2 
+//Pinf for L298P shield
+#define enablePin1 12
+#define brakePin1 9
+#define enablePin2 13
+#define brakePin2 8
+#define LED1Pin 3
+#define LED2Pin 11
+//Determines status of LEDs
+bool led = 0;
+
+
+void setup() {
+//Setting LED1
+pinMode(enablePin1, OUTPUT); 
+pinMode(brakePin1, OUTPUT); 
+digitalWrite(enablePin1, HIGH); 
+digitalWrite(brakePin1, LOW);  
+//Setting LED2
+pinMode(enablePin2, OUTPUT); 
+pinMode(brakePin2, OUTPUT); 
+digitalWrite(enablePin2, HIGH); 
+digitalWrite(brakePin2, LOW);
+//Setting sensor
+pinMode(sensorPin, INPUT_PULLUP);
+attachInterrupt(digitalPinToInterrupt(sensorPin), sensor, FALLING);  //Interrupt.. triggers, whenever switch is toggled
+//turn off led on start
+    digitalWrite(LED1Pin, LOW);
+    digitalWrite(LED2Pin, LOW);
+
+//Serial.begin(9600);
+}
+void LEDControl()
+{
+    digitalWrite(LED1Pin, led);
+    digitalWrite(LED2Pin, led);
+  //  Serial.println(led); 
+}
+void sensor()
+{
+  led = 1;
+     // Serial.println("led"); 
+
+}
+void loop(){
+  if (led == 1)
+  {
+
+    LEDControl();
+    //delay(LEDPulze);
+    delayMicroseconds(LEDPulze);
+    led = 0;
+    LEDControl();
+  }
+//Serial.println(digitalRead(sensorPin));
+}
